a60b1989a25757ea632e6d39ca4244a77dc44784
Paulo Pires <prpires66@gmail.com>
2024-05-07 22:19:49 -0300

    Bugfix CORS

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -1,8 +1,12 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.rsocket.RSocketProperties;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
